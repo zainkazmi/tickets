65ef4ec8bb7fa6f1add1988b1c1eb741b5e56ac6
syedrab <mohibrab@Mohib-Rabs-MacBook-Pro.local>
2014-11-14 22:24:01 -0500

    newchanges

---
+++ company.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="css/themes/smoothness/jquery-ui.css">
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <link rel="stylesheet" href="http://www.priceline.com/hotel/content/css/list/ht-listings.css?v=20140729">
+    <link rel="stylesheet" href="http://www.priceline.com/hotel/content/css/list/listitem.css?v=20141114">
+    <link rel="stylesheet" href="http://www.priceline.com/sharedapps/content/css/pcln_main.css">
+    <link rel="stylesheet" href="http://www.priceline.com/sharedapps/content/css/grid_1010_12col.css">
+    <link rel="stylesheet" href="http://www.priceline.com/hotel/content/css/trans/details.css?v=20141011">
+    
+    <style>
+    .listitem_price_amount{
+        font-size:36px;
+    }
+    </style>
+</head>
+<body>
+    <div class="main_container">
+        <div id="content">
+            <div id="detailsHotelSummary">
+                <div id="detailsHotelSummaryHeader">
+                    <h1 id="detailsHotelSummaryName">
+                        Ontario Traffic Tickets
+                        <span style="position:static; white-space:nowrap; display:block">
+                            <i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-half"></i><i class="icon-star-empty"></i><i class="icon-star-empty"></i>
+                        </span>
+                    </h1>
+                    <span id="detailsHotelSummaryAddress">1234 The Queensway, Etobicoke, ON</span>
+                    <div id="detailsSummaryLeft" class="left">
+                        <img src="http://www.ontariotraffictickets.com/wp-content/themes/ontariotraffictickets.com/images/logo.jpg"/>
+                    </div>
+                    <iframe src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=ott+legal+1234+The+Queensway+toronto&amp;aq=&amp;sll=43.663189,-79.728275&amp;sspn=0.009352,0.010922&amp;ie=UTF8&amp;hq=ott+legal&amp;hnear=1234+The+Queensway,+Toronto,+Toronto+Division,+Ontario+M8Z+1S2,+Canada&amp;t=m&amp;ll=43.621739,-79.524514&amp;spn=0.006295,0.006295&amp;output=embed" width="425" height="350" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+                    
+        </div>
+    </div>
+</body>
+</html>
--- list.php
+++ list.php
@@ -38,45 +38,53 @@ else{
 </head>
 <body>
     <div class="main_container">
-    <?php
-       if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-            ?>
-            <div id="each-company" class="listitem" style="border:1px solid grey; padding:10px;     display:inline-block">
-                <div id="company-name" style="">
-                    <h3>Ontario Traffic Tickets (OTT)</h3>
-                </div>
-                <div id="left-img-box">
-                    <img style="width:30%; padding:10px; float:left" src="http://ontariospeeding.com/wp-content/uploads/2013/12/Speeding-Tickets.jpg">
-                </div>
-                <div id="mid-details" style="float:left">
-                    <span id="address"><?=$row['Name']?></span>
-                    <div class="listitem_stars listitem_stars_4.0">
-                        <span class="listitem_stars_desc">4-Star</span>
-                    </div><br/>
-                    <ul id="features" style="font-size:75%;">
-                        <li>Legal Consultation</li>
-                        <li>Ticket Analysis</li>
-                        <li>Plea bargain negociations</li>
-                    </ul>
-                </div>
-                <div id="mid-details" style="float:left">
-                    <div class="listitem_right_group">
-                        <div class="">
-                            <span class="listitem_price">
-                                <a class="listitem_price_link"> 
-                                    <span class="listitem_price_currency">$</span>
-                                    <span class="listitem_price_amount"><?=$row['price']?></span>
-                                    <span class="listitem_price_unit">/ticket</span>
-                                </a>
-                            </span>
-                            <button type="button" class="listitem_choose" choosebutton="true">Choose</button>
-                    </div> <!-- listitem_price_group -->
-                </div>
+        <div class="group content-padding">
+            <div id="gutter" class="left">
+                <div id="btn-map-view-show" class="side-semi-map" gaclick="Listings_Lineless~Map_Show"> 
+                    <a href="/hotel/listings.do?selectedTab=2&amp;key=i2ibuc8w&amp;jsk=334a050a344b050a201411150200005f4010210733&amp;plf=PCLH">Show Map View</a>
                 </div>
             </div>
-        <?  }
-    } ?>
+            <div id="main-content" class="left">
+            <?php
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                ?>
+                <div id="each-company" class="listitem" style="border:1px solid grey; padding:10px;     display:inline-block">
+                    <div id="company-name" style="">
+                        <h3><a href="company.php">Ontario Traffic Tickets (OTT)</a></h3>
+                    </div>
+                    <div id="left-img-box">
+                        <img style="width:30%; padding:10px; float:left" src="http://ontariospeeding.com/wp-content/uploads/2013/12/Speeding-Tickets.jpg">
+                    </div>
+                    <div id="mid-details" style="float:left">
+                        <span id="address"><?=$row['Name']?></span>
+                        <div class="listitem_stars listitem_stars_4.0">
+                            <span class="listitem_stars_desc">4-Star</span>
+                        </div><br/>
+                        <ul id="features" style="font-size:75%;">
+                            <li>Legal Consultation</li>
+                            <li>Ticket Analysis</li>
+                            <li>Plea bargain negociations</li>
+                        </ul>
+                    </div>
+                    <div id="mid-details" style="float:left">
+                        <div class="listitem_right_group">
+                            <div class="">
+                                <span class="listitem_price">
+                                    <a class="listitem_price_link"> 
+                                        <span class="listitem_price_currency">$</span>
+                                        <span class="listitem_price_amount"><?=$row['price']?></span>
+                                        <span class="listitem_price_unit">/ticket</span>
+                                    </a>
+                                </span>
+                                <button type="button" class="listitem_choose" choosebutton="true">Choose</button>
+                        </div> <!-- listitem_price_group -->
+                    </div>
+                    </div>
+                </div><?
+                }
+            } ?>
+        </div>
     </div>
 </body>
 </html.
