465dca5ba365cb1ff1fde071a8883866d199af45
syedrab <mohibrab@Mohib-Rabs-MacBook-Pro.local>
2014-11-14 21:08:17 -0500

    updated

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+    //include 'header.php';
+    session_start();
+
+    if(isset($_POST['search'])){
+        include('class.search.php');
+        $search = new Search();
+        if($search->didSearch()){
+            header('location:list.php');
+        }else{
+            $search->showErrors();
+        }
+    }
+
+    include ('./class.login.php');
+    $login = new Login();
+    if($login->isLoggedIn2()){
+        echo "Members Area";
+    }
+    else{
+        echo "Wrong Page";
+    }
+
+    
+    $token = $_SESSION['token'] = md5(uniqid(mt_rand(),true));
+    
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,52 +39,50 @@
     </script>
 </head>
 <body>
-    <nav>Home</nav>
-<form method="POST" action="list.php">
-<div class="site-heading">
-    <div id="tabs">
-      <ul>
-        <li><a href="#tabs-1">Speeding</a></li>
-        <li><a href="#tabs-2">Traffic</a></li>
-        <li><a href="#tabs-3">Parking</a></li>
-        <li><a href="#tabs-3">Filing</a></li>
-      </ul>
-      <div id="tabs-1">
-            <h3>Search and Save on Tickets</h3>
-            <label style="display:block">Where did you get the ticket?
-                <input type="text" />
-            </label>
-            <br/>
-            <label style="display:block">
-                Date of Infraction
-                <input type="datepicker">
-            </label>
-            <br/>
+    <form method="POST" action="<?=$_SERVER['PHP_SELF'];?>">    
+    <div class="site-heading">
+        <div id="tabs">
+          <ul>
+            <li><a href="#tabs-1">Speeding</a></li>
+            <li><a href="#tabs-2">Traffic</a></li>
+            <li><a href="#tabs-3">Parking</a></li>
+            <li><a href="#tabs-3">Filing</a></li>
+          </ul>
+          <div id="tabs-1">
+                    <h3>Search and Save on Tickets</h3>
+                    <label style="display:block">Where did you get the ticket?
+                        <input name="cityName" type="text" />
+                    </label>
+                    <br/>
+                    <label style="display:block">
+                        Date of Infraction
+                        <input name="speedInfractionDate" type="datepicker">
+                    </label>
+                    <br/>
 
-            <label style="display:block">
-                <label>Speed
-                <select>
-                    <option value="1">0-15km over</option>
-                    <option value="2">16-29km over</option>
-                    <option value="3">30-49km over</option>
-                    <option value="4">50+ km over</option>
-                </select>
-            </label>
-                              
-            <button>Search Lawyers</button>
-      </div>
-      <div id="tabs-2">
-        <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
-      </div>
-      <div id="tabs-3">
-        <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-        <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
-      </div>
-      <div id="tabs-3">
+                    <label style="display:block">
+                        <label>Speed
+                        <select name="speedLimit">
+                            <option value="1">0-15km over</option>
+                            <option value="2">16-29km over</option>
+                            <option value="3">30-49km over</option>
+                            <option value="4">50+ km over</option>
+                        </select>
+                    </label>
+                    <input type="submit" name="search">Search Lawyers</button>
+          </div>
+          <div id="tabs-2">
+            <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+          </div>
+          <div id="tabs-3">
+            <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+            <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+          </div>
+          <div id="tabs-3">
 
-      </div>
+          </div>
+        </div>
     </div>
-</div>
-</form>
-</body>
-</html>
+    <input type="hidden" name="token" value="<?=$token?>"/>
+    </form>
+<? include 'footer.php';?>
--- list.php
+++ list.php
@@ -1,160 +1,82 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$db = "fred";
+session_start();
+include ('./class.search.php');
 
-// Create connection
-$conn = new mysqli($servername, $username, $password,$db);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT * FROM City";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["Name"]. " " . $row["CountryCode"]. "&nbsp;";
-    }
-} else {
-    echo "0 results";
+$search = new Search();
+if($search->didSearch()){
+    $result = $search->getResults();
 }
-$conn->close();
-?>
+else{
+    echo "Wrong page";
+}?>
+<?php
+       /*                 if ($result->num_rows > 0) {
+                            // output data of each row
+                            while($row = $result->fetch_assoc()) {
+                                echo "<span>$".$row['price']."</span><br/>";
+                                echo "<span class='subheading'>".$row['Name']. ", ". $row['District']." <br/>".$row['CountryCode']."</span>";
+                            }
+                        }*/
+                        ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Clean Blog - About</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/clean-blog.min.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <!-- Navigation -->
-    <div id="main-nav"></div>
+    <link rel="stylesheet" href="css/themes/smoothness/jquery-ui.css">
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <link rel="stylesheet" href="http://www.priceline.com/hotel/content/css/list/ht-listings.css?v=20140729">
+    <link rel="stylesheet" href="http://www.priceline.com/hotel/content/css/list/listitem.css?v=20141114">
+    <link rel="stylesheet" href="http://www.priceline.com/sharedapps/content/css/pcln_main.css">
+    <link rel="stylesheet" href="http://www.priceline.com/sharedapps/content/css/grid_1010_12col.css">
+    
     <style>
-    .nav-tabs>li>a{
-        background-color:#fff;
-        color: #0a84c1;
-    }
-    .nav-tabs>li.active>a{
-        background-color:#eee;
+    .listitem_price_amount{
+        font-size:36px;
     }
     </style>
-
-    <!-- Page Header -->
-    <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-color:#45D2BA">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <div class="page-heading">
-                        <h1>About Me</h1>
-                        <hr class="small">
-                        <span class="subheading">This is what I do.</span>
-                    </div>
+</head>
+<body>
+    <div class="main_container">
+    <?php
+       if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+            ?>
+            <div id="each-company" class="listitem" style="border:1px solid grey; padding:10px;     display:inline-block">
+                <div id="company-name" style="">
+                    <h3>Ontario Traffic Tickets (OTT)</h3>
                 </div>
-            </div>
-        </div>
-    </header>
-
-    <!-- Main Content -->
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum molestiae debitis nobis, quod sapiente qui voluptatum, placeat magni repudiandae accusantium fugit quas labore non rerum possimus, corrupti enim modi! Et.</p>
-            </div>
-        </div>
-    </div>
-
-    <hr>
-
-    <!-- Footer -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <ul class="list-inline text-center">
-                        <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
+                <div id="left-img-box">
+                    <img style="width:30%; padding:10px; float:left" src="http://ontariospeeding.com/wp-content/uploads/2013/12/Speeding-Tickets.jpg">
+                </div>
+                <div id="mid-details" style="float:left">
+                    <span id="address"><?=$row['Name']?></span>
+                    <div class="listitem_stars listitem_stars_4.0">
+                        <span class="listitem_stars_desc">4-Star</span>
+                    </div><br/>
+                    <ul id="features" style="font-size:75%;">
+                        <li>Legal Consultation</li>
+                        <li>Ticket Analysis</li>
+                        <li>Plea bargain negociations</li>
                     </ul>
-                    <p class="copyright text-muted">Copyright &copy; Your Website 2014</p>
+                </div>
+                <div id="mid-details" style="float:left">
+                    <div class="listitem_right_group">
+                        <div class="">
+                            <span class="listitem_price">
+                                <a class="listitem_price_link"> 
+                                    <span class="listitem_price_currency">$</span>
+                                    <span class="listitem_price_amount"><?=$row['price']?></span>
+                                    <span class="listitem_price_unit">/ticket</span>
+                                </a>
+                            </span>
+                            <button type="button" class="listitem_choose" choosebutton="true">Choose</button>
+                    </div> <!-- listitem_price_group -->
+                </div>
                 </div>
             </div>
-        </div>
-    </footer>
-
-    <!-- jQuery -->
-    <script src="js/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="js/clean-blog.min.js"></script>
-
-    <script> 
-        $(function(){
-          $("#main-nav").load("nav.html"); 
-        });
-        $('.datepicker').datepicker();
-
-    </script> 
-
+        <?  }
+    } ?>
+    </div>
 </body>
-
-</html>
+</html.
