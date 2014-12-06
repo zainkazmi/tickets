5be2196c0a741357874201c4f86d543eed753841
Zain Kazmi <zain.uwaterloo@gmail.com>
2014-12-05 20:46:50 -0500

    [New Beginning]
    new layout

---
+++ src/js/script2.js
@@ -0,0 +1,115 @@
+/**
+ * Created by Zain Kazmi on 12/1/2014.
+ */
+//When the document is ready
+$(document).ready(
+                   function(){
+
+                       //when the list group item is clicked
+                       $('.list-group-item').click(
+
+
+
+                                 //another function within click
+                                  function(){
+
+                                      $('#info').remove();
+
+                                      id = $(this).attr('id');
+                                      link = $(this).attr('data');
+
+                                      $.ajax({type: "GET", url: link, dataType: "json", async: false}).done(function(json){
+
+
+
+
+                                         var ticketDetail =    "<div id='info'>"
+                                                               +"<div class='list-group-item'><div class='row'><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Ticket#: </strong>"
+                                                               +json.ticketNumber
+                                                               +"</p></div><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Status: </strong>"
+                                                               +json.status
+                                                               +"</p> </div> <div class='col-xs-3' style='font-size: 13px; color:darkgray'> <p class='list-group-item-text'><strong>Date Created: </strong>"
+                                                               +json.dateCreated
+                                                               +"</p> </div> <div class='col-xs-3' style='font-size: 13px; color:darkgray'> <p class='list-group-item-text'><strong>Type: </strong>"
+                                                               +json.ticketType
+                                                               +"</p></div></div></div><div class='list-group-item'><div class='row'><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Assigned to: </strong>"
+                                                               +json.assignedTo
+                                                               +"</p></div><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Phone: </strong>"
+                                                               +json.lawyerPhone
+                                                               +"</p></div><div class='col-xs-3'style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Email: </strong><a href='/'>"
+                                                               +json.lawyerEmail
+                                                               +"</a></p></div><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Price paid: </strong>"
+                                                               +json.pricePaid
+                                                               +"</span></p></div></div></div><div class='list-group-item'><div class='row'><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Date of Issue: </strong>"
+                                                               +json.dateOfIssue
+                                                               +"</p></div><div class ='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>City of Issue: </strong>"
+                                                               +json.cityOfIssue
+                                                               +"</p></div><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Offence #: </strong>"
+                                                               +json.offenceNumber
+                                                               +"</p></div><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Offence amount: </strong>"
+                                                               +json.offenceAmount
+                                                               +"</p></div></div></div><div class='list-group-item'><div class='row'><div class='col-xs-7' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Offence Details: </strong>"
+                                                               +json.offenceDetails
+                                                               +"</p></div></div></div><div class='list-group-item'><div class='row'><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Driver's Name: </strong>"
+                                                               +json.driversName
+                                                               +"</p></div><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>License #: </strong>"
+                                                               +json.licenseNumber
+                                                               +"</p></div><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>DOB: </strong>"
+                                                               +json.dob
+                                                               +"</p></div><div class='col-xs-3' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Phone: </strong>"
+                                                               +json.phone
+                                                               +"</p></div></div></div><div class='list-group-item' style='font-size: 13px; color:darkgray'><p class='list-group-item-text'><strong>Email: </strong>"
+                                                               +json.email
+                                                               +"</p><p class='list-group-item-text'><strong>Notes: </strong>"
+                                                               +json.notes
+                                                               +"</p></div><div class='list-group-item' id='comment' style='font-size: 13px; color:darkgray; '>"
+                                                               +"<p class='list-group-item-text'><strong>Comments by the lawyer: </strong></p>"
+                                                               +"</div><div class='list-group-item' id='more' style='font-size: 13px; color:darkgray; '>"
+                                                               +"<a class='list-group-item-text' href='ticket.html'><strong>More Details >>> </strong></a>"
+                                                               +"</div></div>";
+
+                                         $("#" + id).after(ticketDetail);
+
+
+                                          if (json.comments.length ==0 ){
+
+                                                  var commentSec =   "<h5 class='list-group-item-text' >Lawyer has not posted anything yet: <em>" + "\"No Updates available at this time\"" + "</em></h5>";
+
+                                                  $('#comment').append(commentSec);
+                                              }
+
+                                          else{
+
+
+                                                  for( var i in json.comments){
+
+                                                      var commentObject = json.comments[i];
+
+
+                                                      var domEl =   "<h5 class='list-group-item-text'>Posted on <strong>" + commentObject.time + "</strong>: <em> \"" + commentObject.comment + "\"</em></h5>";
+
+
+                                                      $('#comment').append(domEl);
+
+                                                  }
+
+
+
+                                          }
+
+
+
+
+
+                                      });
+
+                                 }
+
+
+
+
+                       )
+
+
+                   }
+                 )
---
+++ src/js/script3.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Zain Kazmi on 12/5/2014.
+ */
+$(document).ready(function (){
+
+
+    link = 'http://private-9992f-tickets8.apiary-mock.com/ticket/1';
+    $.ajax({
+        type: "GET",
+        url: link,
+        dataType: "json",
+        async: false
+    })
+        .done(function(json){
+            //console.log(json.ticketNumber)
+
+            //Ticket details update
+            $('#TN').text(json.ticketNumber)
+            $('#ST').text(json.status)
+            $('#DC').text(json.dateCreated)
+            $('#LM').text(json.lastModified)
+            $('#AT').text(json.assignedTo)
+            $('#LP').text(json.lawyerPhone)
+            $('#LE').text(json.lawyerEmail)
+            $('#PP').text(json.pricePaid)
+            $('#DOI').text(json.dateOfIssue)
+            $('#COI').text(json.cityOfIssue)
+            $('#TY').text(json.ticketType)
+            $('#ON').text(json.offenceNumber)
+            $('#OA').text(json.offenceAmount)
+            $('#OD').text(json.offenceDetails)
+            $('#DN').text(json.driversName)
+            $('#DOB').text(json.dob)
+            $('#PH').text(json.phone)
+            $('#EM').text(json.email)
+            $('#LI').text(json.licenseNumber)
+            $('#NO').text(json.notes)
+
+            //ticket details footer
+            $('#LU').text(json.lastModified)
+
+           // $('#comments').empty();
+
+
+
+
+            if (json.comments.length ==0 ){
+
+                var domEl =   "<div class='list-group-item'>" +  "<h5 class='list-group-item-text'><strong> Bart Law Associates</strong> has not posted anything yet.</h5>" +
+                    "<p class='list-group-item-text'><em>" + "No Updates available at this time" + "</em></p></div>";
+
+                $('#comments').html(domEl);
+            }
+
+            else{
+
+                for( var i in json.comments){
+
+                    var commentObject = json.comments[i];
+
+
+                    var domEl =   "<div class='list-group-item'>" +  "<h5 class='list-group-item-text'><strong> Bart Law Associates</strong> commented on <strong>" + commentObject.time + "</strong></h5>" +
+                        "<p class='list-group-item-text'><em>" + commentObject.comment + "</em></p></div>";
+
+                    $('#comments').append(domEl);
+
+                }
+            }
+
+
+
+
+
+
+
+        });}
+);
