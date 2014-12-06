3bcac8363885f5b5fb7b43d1a6c68aabc48d0cc5
syedrab <mohibrab@Mohib-Rabs-MacBook-Pro.local>
2014-12-05 20:44:13 -0500

    [Delete all]

--- info/phpConnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$link = mysql_connect('192.168.30.23', 'root', '');
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
-}
-echo 'Connected successfully';
-mysql_close($link);
-?>
